7efa73928ba9f429b7db738b364d1b9cc93e105e
Maimoona Qasmi <96918798+Maimoona-Qasmi-3@users.noreply.github.com>
2022-11-18 22:20:16 +0500

    Update App.jsx

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -4,26 +4,15 @@ import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
 import Item from "./Item";
-<<<<<<< HEAD
+import { Principal } from "@dfinity/principal";
 
 function App() {
-
   const NFTID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
 
   return (
     <div className="App">
       <Header />
       <Item id={NFTID}/>
-=======
-import { Principal } from "@dfinity/principal";
-
-function App() {
-
-  return (
-    <div className="App">
-      <Header />
-      <Item id="rrkah-fqaaa-aaaaa-aaaaq-cai"/>
->>>>>>> e5e3e7fe2cdb0723117c6e5ec907705981ca72ad
       {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
